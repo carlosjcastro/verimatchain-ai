c054db34ba44d1ebc609f5444e9a95c2d4ad455d
Carlos José Castro Galante <carlosjosecastro.p@gmail.com>
2026-05-09 14:50:55 -0300

    edit: CORS

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ def create_application() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:3000"],
+        allow_origins=["http://localhost:3000", "https://verimatchain-ai.carlosjcastrog.com"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
